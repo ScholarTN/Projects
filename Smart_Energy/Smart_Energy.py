1f5c27d76480155cf034cb1a8e6e99bb2f14822f
THUBALAMI NKOMAZANA <thubalaminkomazana15@gmail.com>
2025-04-05 02:58:04 +0530

    Update Smart_Energy.py

--- Smart_Energy/Smart_Energy.py
+++ Smart_Energy/Smart_Energy.py
@@ -165,4 +165,4 @@ def download_pdf(n_clicks):
 server = app.server  
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8025, host="0.0.0.0")
+    app.run(debug=False, port=8080, host="0.0.0.0")
